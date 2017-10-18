92d77ef21deb0e0e2ea6145e6d155f717b2f09d3
hanjialele925 <2268781158@qq.com>
2017-10-19 07:55:26 +0800

    测试

---
+++ src/Hello.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Hello;
+
+class Hello
+{
+    static public function world()
+    {
+        return 'Hello World!';
+    }
+}
+
+
+
+
+
+
+
+
+/**
+ * Hello.php
+ *
+ * ...
+ *
+ * 2017 Copyright (c)
+ *
+ * 修改历史
+ * ----------------------------------------
+ * 2017/10/18, 作者: 韩佳乐, 操作:创建
+ **/
---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+//use Hello\Hello;
+//echo Hello::world();
+use Hello\Hello;
+echo Hello::world();
+
+
+
+
+
+
+
+
+
+/**
+ * test.php
+ *
+ * ...
+ *
+ * 2017 Copyright (c)
+ *
+ * 修改历史
+ * ----------------------------------------
+ * 2017/10/18, 作者: 韩佳乐, 操作:创建
+ **/
